d9c47577b4f908eb58f3d988e3b4847a739e92c9
duytm19 <tranminhduy1912@gmail.com>
2025-01-04 20:46:26 +0700

    optimiz filter and search

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,44 +1,30 @@
 const Product=require("../../models/product.model")
-
+const filterStatusHelper=require("../../helpers/filterStatus")
+const searchHelper=require("../../helpers/search")
 //[GET] /admin/products
 module.exports.index= async(req,res)=>{
-    let filterStatus=[
-        {
-            name:"Tất cả",
-            status:"",
-            class:""
-        },
-        {
-            name:"Hoạt động",
-            status:"active",
-            class:""
-        },
-        {
-            name:"Dừng hoạt động",
-            status:"inactive",
-            class:""
-        }
-    ]
-
-    if(req.query.status){
-        const index=filterStatus.findIndex(item=>item.status==req.query.status)
-        filterStatus[index].class="active"
-    }
-    else{
-        const index=filterStatus.findIndex(item=>item.status=="")
-        filterStatus[index].class="active"
-    }
+    
+    const filterStatus=filterStatusHelper(req.query)
     let find={
         deleted:false
     }
     if(req.query.status){
         find.status=req.query.status
     }
+
+    const objectSearch=searchHelper(req.query)
+
+    
+    if(objectSearch.regex){
+        find.title=objectSearch.regex
+    }
+
     const products= await Product.find(find)
 
     res.render("admin/pages/products/index",{
        pageTitle:"Trang danh sach product",
        products:products,
-       filterStatus:filterStatus
+       filterStatus:filterStatus,
+       keyword:objectSearch.keyword,
     })
  }
