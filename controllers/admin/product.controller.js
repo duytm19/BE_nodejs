82296fb51921f9fe0e5586fc6eb5bd9c0ff07315
duytm19 <tranminhduy1912@gmail.com>
2025-01-05 13:58:20 +0700

    pagination

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,30 +1,47 @@
 const Product=require("../../models/product.model")
 const filterStatusHelper=require("../../helpers/filterStatus")
 const searchHelper=require("../../helpers/search")
+const paginationHelper=require("../../helpers/pagination")
 //[GET] /admin/products
-module.exports.index= async(req,res)=>{
+module.exports.index = async(req,res)=>{
     
-    const filterStatus=filterStatusHelper(req.query)
+    //filter status
+    const filterStatus = filterStatusHelper(req.query)
     let find={
         deleted:false
     }
     if(req.query.status){
         find.status=req.query.status
     }
+    //End filter status
 
-    const objectSearch=searchHelper(req.query)
-
-    
+    //Search
+    const objectSearch = searchHelper(req.query)
     if(objectSearch.regex){
         find.title=objectSearch.regex
     }
 
-    const products= await Product.find(find)
+    //End Search
+
+    //Pagination
+    const countProducts = await Product.countDocuments(find);
+    let objectPagination=paginationHelper(
+        {
+            limitItems: 4,
+            currentPage: 1
+        },
+        req.query,
+        countProducts
+    )
+   // console.log(objectPagination)
+    //End Pagination
+    const products= await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip)
 
     res.render("admin/pages/products/index",{
        pageTitle:"Trang danh sach product",
        products:products,
        filterStatus:filterStatus,
        keyword:objectSearch.keyword,
+       pagination: objectPagination,
     })
  }
