5d1feeb5c2f12fb046e54dd55b3b82c6a29d4e0f
duytm19 <tranminhduy1912@gmail.com>
2025-01-06 09:09:14 +0700

    add change status products

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const database = require("./config/database");
 const systemConfig=require("./config/system")
+const methodOverride = require('method-override')
 //ROUTER
 const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
@@ -12,7 +13,8 @@ database.connect();
 
 const app = express();
 const port = 3000;
-
+// override POST to PATCH
+app.use(methodOverride('_method'))
 // configure pug into project
 app.set("views", "./views");
 app.set("view engine", "pug");
