b6fa474dd788c5ed68bb3ec0b5721be416104e27
duytm19 <tranminhduy1912@gmail.com>
2025-01-16 14:05:04 +0700

    deploy vercel

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // configure pug into project
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 //flash
@@ -34,7 +34,7 @@ app.use(flash());
 //App Locals Variables
 app.locals.prefixAdmin=systemConfig.prefixAdmin
 
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 routeAdmin(app);
 route(app);
 
