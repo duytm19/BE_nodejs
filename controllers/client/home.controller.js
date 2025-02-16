99014842a6ed7c03bceb16cbbb1a9bc6651b0e59
duytm19 <tranminhduy1912@gmail.com>
2025-02-16 11:59:09 +0700

    Show feature products in home page and products page

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,8 +1,19 @@
-
+const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
 //[GET] /
 module.exports.index = async(req,res)=>{
+    //Feature Product
+    const productsFeatured = await Product.find({
+        featured: "1",
+        deleted: false,
+        status:"active"
+    })
+    const newProducts = productsHelper.priceNewProducts(productsFeatured)
+
+    // console.log(newProducts)
     res.render("client/pages/home/index",{
         pageTitle:"Home Page",
+        productsFeatured:newProducts
     }
     )
 }
---
+++ helpers/products.js
@@ -0,0 +1,6 @@
+module.exports.priceNewProducts = (products)=>{
+    products.forEach(item=>{
+        item.priceNew= (item.price*(100-item.discountPercentage)/100).toFixed(0)
+    })
+    return products
+}
