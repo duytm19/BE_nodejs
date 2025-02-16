05a39d20b7183c7173bc2abbca40c334d21d3ce7
duytm19 <tranminhduy1912@gmail.com>
2025-02-16 14:02:41 +0700

    show products by category in client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,39 +1,62 @@
-
+const ProductCategory = require("../../models/product-category.model");
 const Product = require("../../models/product.model");
-const productsHelper = require("../../helpers/products")
+const productsHelper = require("../../helpers/products");
+const productsCategoryHelper = require("../../helpers/products-category");
 //[GET] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
-    deleted: false
-  }).sort({position:"desc"})
+    deleted: false,
+  }).sort({ position: "desc" });
 
-  const newProducts = productsHelper.priceNewProducts(products)
+  const newProducts = productsHelper.priceNewProducts(products);
   //console.log(newProducts)
   res.render("client/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
-    products:newProducts
+    products: newProducts,
   });
 };
 
-//[GET] /products/detail/:slug
-module.exports.detail = async(req,res)=>{
-    
-  try{
-      const find = {
-          deleted: false,
-          slug: req.params.slug,
-          status:"active"
-      }
-      const product = await Product.findOne(find)
-      res.render("client/pages/products/detail",{
-         pageTitle:product.title,
-         product:product
-      })
+//[GET] /products/detail/:slugProduct
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      slug: req.params.slugProduct,
+      status: "active",
+    };
+    const product = await Product.findOne(find);
+    res.render("client/pages/products/detail", {
+      pageTitle: product.title,
+      product: product,
+    });
+  } catch (error) {
+    req.flash("error", "Can't show detail product ! Please try again");
+    res.redirect(`/products`);
   }
-  catch(error){
-      req.flash("error", "Can't show detail product ! Please try again")
-      res.redirect(`/products`)
-  }
- 
-}
+};
+//[GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    status: "active",
+    deleted: false,
+  });
+  const listSubCategory = await productsCategoryHelper.getSubCategory(
+    category.id
+  );
+
+  const listSubCategoryId = listSubCategory.map((item) => item.id);
+
+  const products = await Product.find({
+    product_category_id: {$in: [category.id,...listSubCategoryId]},
+    deleted: false
+  }).sort({position: "desc"})
+
+  const newProducts = productsHelper.priceNewProducts(products)
+
+  res.render("client/pages/products/index",{
+    pageTitle: category.title,
+    products: newProducts
+  })
+};
