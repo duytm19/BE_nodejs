427c2119620c6613a5f8520df6fde5f2affde6a9
duytm19 <tranminhduy1912@gmail.com>
2025-02-18 23:10:44 +0700

    add products into cart

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+//Show alert
+
+const showAlert = document.querySelector("[show-alert]")
+const closeAlert =document.querySelector("[close-alert]")
+if(showAlert){
+    const time = parseInt(showAlert.getAttribute("data-time"))
+
+    setTimeout(()=>{
+        showAlert.classList.add("alert-hidden")
+    },time)
+
+
+
+    closeAlert.addEventListener("click",()=>{
+        showAlert.classList.add("alert-hidden")
+     })
+}
+
+//End Show alert
