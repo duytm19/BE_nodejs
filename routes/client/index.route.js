67b65c7975b520f0c1f808370ec84c5e978c4ff4
duytm19 <tranminhduy1912@gmail.com>
2025-02-19 18:11:28 +0700

    checkout

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -5,6 +5,7 @@ const homeRoutes = require("./home.route")
 const productRoutes = require("./product.route")
 const searchRoutes = require("./search.route")
 const cartRoutes = require("./cart.route")
+const checkoutRoutes = require("./checkout.route")
 module.exports = (app) => {
 
     app.use(catergoryMiddleware.category)
@@ -16,5 +17,8 @@ module.exports = (app) => {
     app.use("/products", productRoutes)
 
     app.use("/search",searchRoutes)
+
     app.use("/cart",cartRoutes)
+
+    app.use("/checkout", checkoutRoutes)
 }
