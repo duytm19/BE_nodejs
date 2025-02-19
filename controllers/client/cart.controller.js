c3589f39086416290f9ec8088e592dec4ef96f47
duytm19 <tranminhduy1912@gmail.com>
2025-02-19 10:33:52 +0700

    update quantity in cart

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -92,3 +92,23 @@ module.exports.delete = async(req,res)=>{
 
     res.redirect("back")
 }
+
+// [GET] /cart/update/:productId/:quantity
+module.exports.update = async(req,res)=>{
+    const cartId = req.cookies.cartId;
+    const productId = req.params.productId
+    const quantity = req.params.quantity
+    await Cart.updateOne({
+        _id:cartId,
+        'products.product_id': productId
+    },{
+        '$set':{
+            "products.$.quantity": quantity
+        }
+    })
+    req.flash(
+        "success","Update quantity of product in cart successfully!!"
+    )
+
+    res.redirect("back")
+}
---
+++ public/js/cart.js
@@ -0,0 +1,14 @@
+// Update quantity of product in cart
+const inputsQuantity = document.querySelectorAll("input[name='quantity']")
+if(inputsQuantity.length>0){
+    inputsQuantity.forEach(input =>{
+        input.addEventListener("change",()=>{
+            const productId = input.getAttribute("product-id")
+            const quantity = input.value
+
+            window.location.href = `/cart/update/${productId}/${quantity}`
+        })
+        
+    })
+}
+// End Update quantity of product in cart
