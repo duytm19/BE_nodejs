521163afa02b864df2293b0553aa18d3a8ecf0a0
duytm19 <tranminhduy1912@gmail.com>
2025-02-23 13:18:04 +0700

    setting general (admin)

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -6,6 +6,7 @@ const roleRoutes = require("./role.route")
 const accountRoutes = require("./account.route")
 const authRoutes = require("./auth.route")
 const myAccountRoutes = require("./my-account.route")
+const settingRoutes = require('./setting.route')
 
 const authMiddleware = require("../../middlewares/admin/auth.middleware")
 module.exports = (app) => {
@@ -24,5 +25,7 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + "/auth", authRoutes)
 
   app.use(PATH_ADMIN + "/my-account", authMiddleware.requireAuth, myAccountRoutes)
+
+  app.use(PATH_ADMIN + "/settings", authMiddleware.requireAuth, settingRoutes)
 
 };
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,6 +1,8 @@
 const catergoryMiddleware = require("../../middlewares/client/category.middleware")
 const cartMiddleware =require('../../middlewares/client/cart.middleware')
 const userMiddleware = require ('../../middlewares/client/user.middleware')
+const settingMiddleware = require('../../middlewares/client/setting.middleware')
+
 
 const homeRoutes = require("./home.route")
 const productRoutes = require("./product.route")
@@ -8,6 +10,7 @@ const searchRoutes = require("./search.route")
 const cartRoutes = require("./cart.route")
 const checkoutRoutes = require("./checkout.route")
 const userRoutes = require('./user.route')
+
 module.exports = (app) => {
 
     app.use(catergoryMiddleware.category)
@@ -15,6 +18,8 @@ module.exports = (app) => {
     app.use(cartMiddleware.cartId)
 
     app.use(userMiddleware.infoUser)
+
+    app.use(settingMiddleware.settingGeneral)
 
     app.use("/", homeRoutes)
 
