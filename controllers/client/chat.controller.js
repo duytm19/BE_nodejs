a5ab31df4ff0dc8b32fe586b90d02d8d01b937e0
duytm19 <tranminhduy1912@gmail.com>
2025-02-27 17:36:14 +0700

    client send message

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,13 +1,38 @@
+const Chat = require('../../models/chat.model')
+const User = require("../../models/user.model")
+
 //[GET] /chat/
 
 module.exports.index= async (req,res)=>{
+    const userId = res.locals.user.id  
     //Socket IO
-    _io.on('connection',(socket)=>{
-        console.log("a user connected",socket.id)
+    _io.once('connection',(socket)=>{
+        socket.on("CLIENT_SEND_MESSAGE", async (content)=>{
+            const chat = new Chat({
+                user_id: userId,
+                content: content
+            })
+            await chat.save()
+        })
     })
     //End Socket IO
 
+    // Get data messsage from database
+
+    const chats = await Chat.find({
+        deleted:false
+    })
+    for(const chat of chats){
+        const infoUser = await User.findOne({
+            _id: chat.user_id
+        }).select("fullName")
+        chat.infoUser = infoUser
+    }
+
+    // End Get data messsage from database
+
     res.render("client/pages/chat/index",{
-        pageTitle: 'Chat'
+        pageTitle: 'Chat',
+        chats: chats
     })
 }
