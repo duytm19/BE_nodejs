e2e3ceeeb4e7f579f47d583aff0afea9b1632028
duytm19 <tranminhduy1912@gmail.com>
2025-03-05 11:27:33 +0700

    status online real-time

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -83,6 +83,12 @@ module.exports.loginPost = async (req,res)=>{
     },{
         statusOnline:"online"
     })
+    _io.once("connection",(socket)=>{
+        socket.broadcast.emit("SERVER_RETURN_USER_STATUS_ONLINE",{
+            userId: user.id,
+            status:"online"
+        })
+    })
     res.redirect("/")
 }
 //[GET] /user/logout
@@ -92,7 +98,12 @@ module.exports.logout = async (req,res) =>{
     },{
         statusOnline:"offline"
     })
-
+    _io.once("connection",(socket)=>{
+        socket.broadcast.emit("SERVER_RETURN_USER_STATUS_ONLINE",{
+            userId: res.locals.user.id,
+            status:"offline"
+        })
+    })
     res.clearCookie("tokenUser")
     res.clearCookie("cartId")
     res.redirect("/")
